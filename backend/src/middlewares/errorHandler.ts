2920abdb10064a1a962cbbd3033ef8f596283a93
Iv44n <ivandev2oo6@gmail.com>
2024-10-19 22:50:45 -0500

    add Zod error handling to errorHandler

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,9 +1,25 @@
-import { ErrorRequestHandler } from 'express'
-import { logger } from '../utils/logger'
+import { ErrorRequestHandler, Response } from 'express'
+import { INTERNAL_SERVER_ERROR, UNPROCESSABLE_CONTENT } from '../constants/http'
+import { z } from 'zod'
 
-export const errorHandler: ErrorRequestHandler = (err, _req, res): any => {
-  logger.error(err.message)
-  return res.status(500).send('Internal Server Error')
+const zodErrorHandler = (err: z.ZodError, res: Response) => {
+  const errors = err.issues.map((issue) => ({
+    path: issue.path.join('.'),
+    message: issue.message
+  }))
+
+  return res.status(UNPROCESSABLE_CONTENT).send({
+    errors,
+    message: JSON.parse(err.message)
+  })
+}
+
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next): any => {
+  if (err instanceof z.ZodError){
+    return zodErrorHandler(err, res)
+  }
+
+  return res.status(INTERNAL_SERVER_ERROR).send('Internal Server Error')
 }
 
 export default errorHandler
