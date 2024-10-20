03d702a75392b0d4c6f0a28d1a47b5bf9183ff60
Iv44n <ivandev2oo6@gmail.com>
2024-10-19 22:51:19 -0500

    filter sensitive data in requestLogger

--- backend/src/middlewares/requestLogger.ts
+++ backend/src/middlewares/requestLogger.ts
@@ -2,6 +2,15 @@ import { RequestHandler } from 'express'
 import { logger } from '../utils/logger'
 import pc from 'picocolors'
 
+const filterSensitiveData = (body: any) => {
+  const filteredBody = { ...body }
+
+  if(body.password) filteredBody.password = '***********'
+  if(body.confirmPassword) filteredBody.confirmPassword = '************'
+
+  return filteredBody
+}
+
 const requestLogger: RequestHandler = (req, _res, next) => {
   const timestamp = new Date().toISOString()
 
@@ -10,7 +19,7 @@ const requestLogger: RequestHandler = (req, _res, next) => {
     ${pc.bold('Timestamp:')} ${pc.yellow(timestamp)}
     ${pc.bold('Method:')}    ${pc.green(pc.italic(req.method))}
     ${pc.bold('Path:')}      ${pc.green(req.path)}
-    ${pc.bold('Body:')}      ${pc.magenta(JSON.stringify(req.body, null, 2) || '{}')}
+    ${pc.bold('Body:')}      ${pc.magenta(JSON.stringify(filterSensitiveData(req.body), null, 2) || '{}')}
     ${pc.bold(pc.cyan('================================='))}
   `)
 
