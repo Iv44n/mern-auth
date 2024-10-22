edf1e4c166f0ec96d17ee705b1c383b8fd7f46d1
Iv44n <ivandev2oo6@gmail.com>
2024-10-22 18:57:40 -0500

    add session creation and JWT generation on user registration

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,6 +1,10 @@
+import Audience from '../constants/audience'
+import { JWT_SECRET } from '../constants/env'
 import { CONFLICT } from '../constants/http'
-import userModel from '../models/user.model'
+import SessionModel from '../models/session.model'
+import UserModel from '../models/user.model'
 import AppError from '../utils/AppError'
+import jwt from 'jsonwebtoken'
 
 interface createAcountParams {
   email: string,
@@ -10,7 +14,7 @@ interface createAcountParams {
 }
 
 export const createAcount = async (data: createAcountParams) => {
-  const existingUser = await userModel.exists({
+  const existingUser = await UserModel.exists({
     username: data.username,
     email: data.email
   })
@@ -19,11 +23,33 @@ export const createAcount = async (data: createAcountParams) => {
     throw new AppError(CONFLICT, 'User or email already in use')
   }
 
-  const user = await userModel.create({
+  const user = await UserModel.create({
     username: data.username,
     email: data.email,
     password: data.password
   })
 
-  return user.omitPassword()
+  const userId = user._id
+
+  const session = await SessionModel.create({
+    userId,
+    userAgent: data.userAgent
+  })
+
+  const accessToken = jwt.sign(
+    {
+      userId,
+      sessionId: session._id
+    },
+    JWT_SECRET,
+    {
+      expiresIn: '10m',
+      audience: [Audience.User]
+    }
+  )
+
+  return {
+    user: user.omitPassword(),
+    accessToken
+  }
 }
