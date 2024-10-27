04380384f8d448203212b8834035a7b88f12d751
Iv44n <ivandev2oo6@gmail.com>
2024-10-27 11:22:01 -0500

    feat: add loginHandler for user authentication with cookies

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express'
 import { CREATED } from '../constants/http'
-import { registerSchema } from '../schemas/auth.schema'
-import { createAcount } from '../services/auth.service'
+import { loginSchema, registerSchema } from '../schemas/auth.schema'
+import { createAcount, loginUser } from '../services/auth.service'
 import AppError from '../utils/AppError'
 import { tenMinutesFromNow, thirtyDaysFromNow } from '../utils/date'
 
@@ -13,6 +13,41 @@ export const registerHandler: RequestHandler = async (req, res, next): Promise<a
     })
 
     const { user, accessToken, refreshToken } = await createAcount(request)
+
+    return res
+      .cookie('accessToken', accessToken, {
+        sameSite: 'strict',
+        httpOnly: true,
+        secure: false,
+        expires: tenMinutesFromNow()
+      })
+      .cookie('refreshToken', refreshToken, {
+        sameSite: 'strict',
+        httpOnly: true,
+        secure: false,
+        expires: thirtyDaysFromNow(),
+        path: '/auth/refresh'
+      })
+      .status(CREATED).json(user)
+
+  } catch (error) {
+    if(error instanceof AppError){
+      return res.status(error.statusCode).json({ error: error.message })
+    }
+
+    return next(error)
+  }
+}
+
+export const loginHandler: RequestHandler = async (req, res, next): Promise<any> => {
+  try {
+
+    const request = loginSchema.parse({
+      ...req.body,
+      userAgent: req.headers['user-agent']
+    })
+
+    const { user, accessToken, refreshToken } = await loginUser(request)
 
     return res
       .cookie('accessToken', accessToken, {
