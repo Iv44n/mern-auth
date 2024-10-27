b9762a2b2bbe8c33c1615cf9ed821388c77fad24
Iv44n <ivandev2oo6@gmail.com>
2024-10-27 11:23:17 -0500

    feat: add loginUser function for user authentication

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import Audience from '../constants/audience'
 import { JWT_REFRESH_SECRET, JWT_SECRET } from '../constants/env'
-import { CONFLICT } from '../constants/http'
+import { CONFLICT, UNAUTHORIZED } from '../constants/http'
 import SessionModel from '../models/session.model'
 import UserModel from '../models/user.model'
 import AppError from '../utils/AppError'
@@ -34,6 +34,64 @@ export const createAcount = async (data: createAcountParams) => {
   const session = await SessionModel.create({
     userId,
     userAgent: data.userAgent
+  })
+
+  const refreshToken = jwt.sign(
+    {
+      sessionId: session._id
+    },
+    JWT_REFRESH_SECRET,
+    {
+      expiresIn: '30d',
+      audience: [Audience.User]
+    }
+  )
+
+  const accessToken = jwt.sign(
+    {
+      userId,
+      sessionId: session._id
+    },
+    JWT_SECRET,
+    {
+      expiresIn: '10m',
+      audience: [Audience.User]
+    }
+  )
+
+  return {
+    user: user.omitPassword(),
+    refreshToken,
+    accessToken
+  }
+}
+
+interface loginParams {
+  email: string
+  password: string
+  userAgent?: string
+}
+
+export const loginUser = async (data: loginParams) => {
+  const { email, password, userAgent } = data
+
+  const user = await UserModel.findOne({ email })
+
+  if(!user){
+    throw new AppError(UNAUTHORIZED, 'Invalid email or password')
+  }
+
+  const isValidPassword = await user.comparePassword(password)
+
+  if(!isValidPassword){
+    throw new AppError(UNAUTHORIZED, 'Invalid email or password')
+  }
+
+  const userId = user._id
+
+  const session = await SessionModel.create({
+    userId,
+    userAgent
   })
 
   const refreshToken = jwt.sign(
