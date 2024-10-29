88714ce03b7b0c0b78e681db06f99d20f78acc53
Iv44n <ivandev2oo6@gmail.com>
2024-10-28 20:37:23 -0500

    feat(auth): add setAuthCookies helper for token cookies

---
+++ backend/src/utils/cookies.ts
@@ -0,0 +1,34 @@
+import { CookieOptions, Response } from 'express'
+import { tenMinutesFromNow, thirtyDaysFromNow } from './date'
+import { NODE_ENV } from '../constants/env'
+
+const REFRESH_PATH = '/auth/refresh'
+const secure = NODE_ENV !== 'development'
+
+const defaultsTokenCookieOptions: CookieOptions = {
+  sameSite: 'strict',
+  httpOnly: true,
+  secure
+}
+
+const accessTokenCookieOptions: CookieOptions = {
+  ...defaultsTokenCookieOptions,
+  expires: tenMinutesFromNow()
+}
+
+const refreshTokenCookieOptions: CookieOptions = {
+  ...defaultsTokenCookieOptions,
+  expires: thirtyDaysFromNow(),
+  path: REFRESH_PATH
+}
+
+interface Params {
+  res: Response
+  accessToken: string
+  refreshToken: string
+}
+
+export const setAuthCookies = ({ res, accessToken, refreshToken }: Params) =>
+  res
+    .cookie('accessToken', accessToken, accessTokenCookieOptions)
+    .cookie('refreshToken', refreshToken, refreshTokenCookieOptions)
