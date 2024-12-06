f4f44e0b2645c504e5a2a5794e2ea3a0ba546041
Iv44n <ivandev2oo6@gmail.com>
2024-12-06 16:18:29 -0500

    feat: enhance authentication handler with detailed error messages

--- backend/src/middlewares/authenticateHandler.ts
+++ backend/src/middlewares/authenticateHandler.ts
@@ -8,13 +8,17 @@ const autenticateHandler: RequestHandler = (req, res, next) => {
   const accessToken = req.cookies.accessToken as string | undefined
 
   if (!accessToken) {
-    throw new AppError(UNAUTHORIZED, 'Missing access token')
+    throw new AppError(UNAUTHORIZED, 'Missing access token', 'InvalidAccessToken')
   }
 
   const { payload, error } = verifyToken(accessToken, { secret: JWT_SECRET })
 
   if (error) {
-    throw new AppError(UNAUTHORIZED, 'Invalid access token')
+    throw new AppError(
+      UNAUTHORIZED,
+      error === 'jwt expired' ? 'Token expired' : 'Invalid token',
+      'InvalidAccessToken'
+    )
   }
 
   req.userId = payload?.userId
