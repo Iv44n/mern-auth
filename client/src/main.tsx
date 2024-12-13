8b34995f2f22e13ab1afe43e0e3fe615bfee8ba1
Iv44n <ivandev2oo6@gmail.com>
2024-12-12 21:31:13 -0500

    feat: integrate React Router, React Query, and ReactQueryDevtools in App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,16 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import App from './App'
+import { QueryClientProvider } from '@tanstack/react-query'
+import queryClient from './config/queryClient'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')!).render(
-  <>
-    <h1>Hello</h1>
-  </>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 )
