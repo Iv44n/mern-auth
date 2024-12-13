911239531e4243b98474c34408a1b0bd18b1829b
Iv44n <ivandev2oo6@gmail.com>
2024-12-13 15:13:04 -0500

    feat: add Axios interceptors for automatic token refresh and error handling

---
+++ client/src/config/apiClient.ts
@@ -0,0 +1,35 @@
+import axios, { CreateAxiosDefaults } from 'axios'
+import queryClient from './queryClient'
+import { navigateTo } from '@/lib/navigation'
+
+const options: CreateAxiosDefaults = {
+  baseURL: 'http://localhost:3000',
+  withCredentials: true
+}
+
+const API = axios.create(options)
+const TokenRefreshClient = axios.create(options)
+
+API.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const { config: originalRequest, response } = error
+    const { status, data } = response || {}
+
+    if (status === 401 && data?.errorCode === 'InvalidAccessToken') {
+      try {
+        await TokenRefreshClient.get('/auth/refresh')
+        return TokenRefreshClient(originalRequest)
+      } catch {
+        queryClient.clear()
+        navigateTo('/login', {
+          state: { redirectUrl: window.location.pathname }
+        })
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+export default API
