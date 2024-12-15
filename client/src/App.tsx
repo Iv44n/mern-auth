7f54f55fc8569ad8c2d9d5ef2f0d00ffc3b18868
Iv44n <ivandev2oo6@gmail.com>
2024-12-14 21:19:23 -0500

    feat: add Settings page route to protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import ProtectedRoutes from './components/ProtectedRoutes'
 import HomeProfile from './pages/Home'
 import RegisterForm from './pages/Register'
 import { setNavigate } from './lib/navigation'
+import Settings from './pages/Settings'
 
 export default function App() {
   const navigate = useNavigate()
@@ -13,6 +14,7 @@ export default function App() {
     <Routes >
       <Route path='/' element={<ProtectedRoutes />}>
         <Route index element={<HomeProfile />} />
+        <Route path='/settings' element={<Settings />} />
       </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<RegisterForm />} />
