7e2d019370912815deba0c0c6617eb3a6cee5b10
Iv44n <ivandev2oo6@gmail.com>
2024-12-14 21:22:20 -0500

    refactor: move layout to HomeLayout component in HomeProfile

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,17 +3,16 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { AlertTriangle, Mail, UserIcon, Calendar } from 'lucide-react'
 import useAuth from '@/hooks/useAuth'
 import UserInfoItem from '@/components/UserInfoItem'
-import UserMenu from '@/components/UserMenu'
+import HomeLayout from '@/components/HomeLayout'
 
 const formatDate = (date: string) => new Date(date).toLocaleString()
 
 export default function HomeProfile() {
   const { user } = useAuth()
-  const { username, email, verified, _id: userId, createdAt } = user
+  const { username, email, verified, _id: userId, createdAt } = user || {}
 
   return (
-    <main className='px-4 py-8 bg-gradient-to-br from-zinc-900 to-black w-full h-dvh'>
-      <UserMenu username={username} />
+    <HomeLayout username={username || ''}>
       <Card className='max-w-2xl mx-auto mt-16 bg-transparent border-none'>
         <CardHeader className='pb-4'>
           <CardTitle className='text-3xl text-zinc-100 text-center'>
@@ -22,9 +21,9 @@ export default function HomeProfile() {
         </CardHeader>
         <CardContent className='space-y-6'>
           <div className='space-y-6'>
-            <UserInfoItem icon={<Mail className='h-6 w-6 text-zinc-400' />} label='Email' value={email} />
-            <UserInfoItem icon={<UserIcon className='h-6 w-6 text-zinc-400' />} label='User ID' value={userId} />
-            <UserInfoItem icon={<Calendar className='h-6 w-6 text-zinc-400' />} label='Join Date' value={formatDate(createdAt)} />
+            <UserInfoItem icon={<Mail className='h-6 w-6 text-zinc-400' />} label='Email' value={email || ''} />
+            <UserInfoItem icon={<UserIcon className='h-6 w-6 text-zinc-400' />} label='User ID' value={userId || ''} />
+            <UserInfoItem icon={<Calendar className='h-6 w-6 text-zinc-400' />} label='Join Date' value={formatDate(createdAt || '')} />
           </div>
 
           {!verified && (
@@ -38,6 +37,6 @@ export default function HomeProfile() {
           )}
         </CardContent>
       </Card>
-    </main>
+    </HomeLayout>
   )
 }
