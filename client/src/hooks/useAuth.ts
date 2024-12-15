c8e6087162b6763f0ea8b3c0ad9234917f3be8a2
Iv44n <ivandev2oo6@gmail.com>
2024-12-14 21:17:57 -0500

    refactor: simplify useAuth hook by removing redundant type definition

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,15 +1,6 @@
 import { getUser } from '@/lib/api'
 import { useQuery } from '@tanstack/react-query'
 
-interface User {
-  _id: string
-  username: string
-  email: string
-  verified: boolean
-  createdAt: string
-  updatedAt: string
-}
-
 const useAuth = () => {
   const { data, ...rest } = useQuery({
     queryKey: ['auth'],
@@ -17,10 +8,8 @@ const useAuth = () => {
     staleTime: Infinity
   })
 
-  const user = data?.data as User
-
   return {
-    user,
+    user: data,
     ...rest
   }
 }
