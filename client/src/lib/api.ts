f648d852bbcca1d62013bdb1cff0868e07c3958f
Iv44n <ivandev2oo6@gmail.com>
2024-12-14 21:17:25 -0500

    refactor: add session and user types, update API functions

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -10,7 +10,26 @@ interface RegisterParams extends AuthParams {
   confirmPassword: string
 }
 
-export const login = (data: AuthParams) => API.post('/auth/login', data)
-export const register = (data: RegisterParams) => API.post('/auth/register', data)
-export const getUser = () => API.get('/user')
-export const logout = () => API.get('/auth/logout')
+interface Session {
+  _id: string
+  userAgent: string
+  createdAt: string
+  expiresAt: string,
+  isCurrent?: boolean
+}
+
+interface User {
+  _id: string
+  username: string
+  email: string
+  verified: boolean
+  createdAt: string
+  updatedAt: string
+}
+
+export const login = async (data: AuthParams) => await API.post('/auth/login', data)
+export const register = async (data: RegisterParams) => await API.post('/auth/register', data)
+export const getUser = async (): Promise<User> => await API.get('/user')
+export const logout = async () => await API.get('/auth/logout')
+export const getSessions = async (): Promise<Session[]> => await API.get('/sessions')
+export const deleteSession = async (id: string) => await API.delete(`/sessions/${id}`)
